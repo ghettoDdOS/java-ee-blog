9508f3aa699e3fd248b824f062320a78485ffca8
penkhasoveg <pen.egor2002@gmail.com>
2023-05-02 14:24:43 +0300

    feat: person servlet

---
+++ src/main/java/controller/PersonsServlet.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PersonsServlet
+ */
+@WebServlet("/HelloPersonsServlet")
+public class PersonsServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public PersonsServlet() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    response.setContentType("text/html");
+    PrintWriter writer = response.getWriter();
+    try {
+      writer.println("<h2>Привет PersonsServlet</h2>");
+    } finally {
+      writer.close();
+    }
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request,
+   *      HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+}
